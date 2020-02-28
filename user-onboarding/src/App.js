a7113f35a2a8767016addd1d892660c5aba40e2f
NaJthaGod <35042135+NaJthaGod@users.noreply.github.com>
2020-02-27 20:40:29 -0500

    set-up

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FormPage from './Form.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       
+       <FormPage />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+ function FormPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FormPage;
